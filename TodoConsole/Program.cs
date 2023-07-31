6dce85be74237d95e4a4300907c98f30b2139871
naaaah <107448004+naaaahTheDev@users.noreply.github.com>
2023-07-31 21:11:52 +0800

    added "mark item as complete" feature.

--- TodoConsole/Program.cs
+++ TodoConsole/Program.cs
@@ -10,8 +10,9 @@ namespace TodoConsole
 		{
 			Back = 0,
 			AddTodo = 1,
-			ViewTodo = 2,
-			DeleteTodo = 3,
+			DeleteTodo = 2,
+			MarkDone = 3,
+			ViewTodo = 4,
 			Exit
 		}
 		static void Main(string[] args)
@@ -20,33 +21,46 @@ namespace TodoConsole
 			List<string> todoList = LoadTodoList(fileName);
 			while (true)
 			{
-				
+
 				// Display the possible choices
 				Console.WriteLine("[1] Add Todo");
-				Console.WriteLine("[2] View Todo List");
-				Console.WriteLine("[3] Delete Todo");
-				Console.WriteLine("[4] Exit");
+				Console.WriteLine("[2] Delete Todo");
+				Console.WriteLine("[3] Mark Task as Done");
+				Console.WriteLine("[4] View Todo List");
+				Console.WriteLine("[5] Exit");
 				Console.Write("Enter Option: ");
 				// Get the choice
 				try
 				{
 					int choice = int.Parse(Console.ReadLine());
 
+					// Add todo
 					if (choice == (int)UserChoice.AddTodo)
 					{
-						Console.Write("Enter task: ");
-						string task = Console.ReadLine();
-						todoList.Add(task);
-						Console.Clear();
-						Console.WriteLine("Task added successfully!");
-						Console.WriteLine("");
-						Console.WriteLine("[0] Back To Menu");
-						int back = int.Parse(Console.ReadLine());
-						if (back == (int)UserChoice.Back)
+						while (true)
 						{
+							Console.Write("Enter task: ");
+							string task = Console.ReadLine();
+							todoList.Add(task);
 							Console.Clear();
+							Console.WriteLine("Task added successfully!");
+							Console.WriteLine("");
+							Console.WriteLine("[0] Back To Main Menu, Add another task [1]");
+							int option = int.Parse(Console.ReadLine());
+							if (option == (int)UserChoice.Back)
+							{
+								Console.Clear();
+								break;
+							}
+							else
+							{
+								Console.Clear();
+								continue;
+							}
 						}
+
 					}
+					// Delete todo
 					else if (choice == (int)UserChoice.DeleteTodo)
 					{
 						while (true)
@@ -58,7 +72,7 @@ namespace TodoConsole
 							{
 								for (int i = 0; i < todoList.Count; i++)
 								{
-									Console.WriteLine(i + 1 + ": " + todoList[i]);
+									Console.WriteLine(i + ": " + todoList[i]);
 									Console.WriteLine("");
 								}
 								Console.WriteLine("Enter the index of the todo task: ");
@@ -67,7 +81,7 @@ namespace TodoConsole
 								{
 									if (indexToDelete >= 0)
 									{
-										todoList.RemoveAt(indexToDelete - 1);
+										todoList.RemoveAt(indexToDelete);
 										Console.Clear();
 										Console.WriteLine("Task deleted successfully!");
 										Console.WriteLine("Back to Main Menu [0], Delete more items [1]");
@@ -76,7 +90,7 @@ namespace TodoConsole
 										{
 											Console.Clear();
 											break;
-										} 
+										}
 										else
 										{
 											Console.Clear();
@@ -88,16 +102,78 @@ namespace TodoConsole
 										Console.WriteLine("Task not found in the todo list.");
 										Console.Clear();
 									}
-								} catch (Exception)
-								{
-									Console.WriteLine("Looks like there was an issue while deleting that task, please try again. ");
-									Console.Clear();
 								}
+								catch (Exception)
+								{
+									Console.Clear();
+									Console.WriteLine("Either, you have no current tasks or there was an error while deleting the task. Please try again.");
+									break;
+								}
+							} 
+							else
+							{
+								Console.Clear();
+								Console.WriteLine("Either, you have no current tasks or there was an error while deleting the task. Please try again.");
+								break;
+							}
 						}
-						}
-						
-						
+
+
 					}
+					// Mark as Done
+					else if (choice == (int)UserChoice.MarkDone)
+					{
+						while (true)
+						{
+							Console.Clear();
+							Console.WriteLine("Todo list: ");
+
+							if (todoList.Count > 0)
+							{
+								for (int i = 0; i < todoList.Count; i++)
+								{
+									Console.WriteLine(i + ": " + todoList[i]);
+									Console.WriteLine("");
+								}
+								Console.WriteLine("Enter the index of the todo task: ");
+								int indexToMark = int.Parse(Console.ReadLine());
+								try
+								{
+									if (indexToMark >= 0)
+
+									todoList.Add(todoList[indexToMark] + " (Completed)");
+									todoList.RemoveAt(indexToMark);
+									Console.Clear();
+									Console.WriteLine("Task marked as done successfully!");
+									Console.WriteLine("Back to Main Menu [0], Mark more items as complete [1]");
+									int option = int.Parse(Console.ReadLine());
+									if (option == (int)UserChoice.Back)
+									{
+										Console.Clear();
+										break;
+									}
+									else
+									{
+										Console.Clear();
+										continue;
+									}
+								}
+								catch (Exception)
+								{
+									Console.Clear();
+									Console.WriteLine("Either, you have no current tasks or there was an error while marking the task. Please try again.");
+									break;
+								}
+							} 
+							else
+							{
+								Console.Clear();
+								Console.WriteLine("Either, you have no current tasks or there was an error while marking the task. Please try again.");
+								break;
+							}
+						}
+					}
+					// View todo
 					else if (choice == (int)UserChoice.ViewTodo)
 					{
 						if (todoList.Count > 0)
@@ -129,6 +205,7 @@ namespace TodoConsole
 							}
 						}
 					}
+					// Exit app
 					else if (choice == (int)UserChoice.Exit)
 					{
 						SaveTodoList(todoList, fileName);
